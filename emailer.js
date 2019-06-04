4a2028658aefdadc558cba0ba2c98c439f69b14b
McPoet <queryfish@gmail.com>
2019-06-04 23:33:45 +0800

    email done

--- emailer.js
+++ emailer.js
@@ -1,5 +1,10 @@
 var nodemailer = require('nodemailer');
 var fs = require('fs');
+var creds = require('./creds');
+var Configs = require('./configs');
+const process = require('process');
+
+
 
 var mailOptions = {
   from: 'marrowsky@126.com',
@@ -13,32 +18,37 @@ var mailTransport = nodemailer.createTransport({
   secure: true,
   port: 994,
   auth: {
-    user: '',
-    pass: ''
+    user: creds.mailuser,
+    pass: creds.mailpass
   }
 });
 
-mailTransport.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
 
-// fs.readFile("./社会工程.mobi", function (err, data) {
-//     mailTransport.sendMail({
-//         sender: 'marrowsky@126.com',
-//         to: 'marrowsky_50@kindle.cn, mcpoet@126.com',
-//         subject: 'Attachment!',
-//         body: 'mail content...',
-//         attachments: [{'filename': 'temp.mobi', 'content': data}]
-//     }, function(err, success) {
-//         if (err) {
-//             // Handle error
-//             console.log(err);}
-//         });
-//     if(err)console.log(err);
+// mailTransport.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
 //   }
-//
-// );
+// });
+
+const logname = process.argv[2];
+const logfile = Configs.workingPath+process.argv[2];
+
+fs.readFile(logfile, function (err, data) {
+    mailTransport.sendMail({
+        sender: creds.mailuser,
+        to: 'mcpoet@126.com',
+        subject: 'crawler.log',
+        body: data+"?",
+        attachments: [{'filename': logname+'.txt', 'content': data}]
+
+    }, function(err, success) {
+        if (err) {
+            // Handle error
+            console.log(err);}
+        });
+    if(err)console.log(err);
+  }
+
+);
