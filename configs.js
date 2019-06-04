c4dec687049860351329064156f047cfef8c06e3
McPoet <queryfish@gmail.com>
2019-06-04 23:42:25 +0800

    .

--- configs.js
+++ configs.js
@@ -2,6 +2,7 @@ module.exports = {
   crawlStep: 50,
   dbUrl:'mongodb://localhost/sobooks',
   // cookieFile:'/home/steve/puppy/cookieJar'
-  cookieFile:'./cookieJar'
+  cookieFile:'./cookieJar',
+  workingPath:'./'
 
 };
