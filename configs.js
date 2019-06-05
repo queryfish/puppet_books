d6160ef5ed641d3a930e72775f2a8a65ba7d7f0f
McPoet <queryfish@gmail.com>
2019-06-05 10:11:41 +0800

    what the fuck is wrong with gitignore

--- configs.js
+++ configs.js
@@ -1,13 +1,7 @@
 module.exports = {
   crawlStep: 20,
   dbUrl:'mongodb://localhost/sobooks',
-<<<<<<< HEAD
   cookieFile:'./cookieJar',
   workingPath:'./'
-=======
-  cookieFile:'/home/steve/puppy/cookieJar',
-  //cookieFile:'./cookieJar'
-  workingPath:'/home/steve/puppy/'
->>>>>>> 46c5debfb4da814ac926f9878d52e5950f15fd1c
 
 };
