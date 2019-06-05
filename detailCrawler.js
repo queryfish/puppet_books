686f3a695f855ba823e389f2d0dc1cc3ed38ae11
McPoet <queryfish@gmail.com>
2019-06-05 10:49:58 +0800

    wtf is with detailcrawler

--- detailCrawler.js
+++ detailCrawler.js
@@ -56,13 +56,13 @@ async function crawl(page, detailUrl)
  await page.waitFor(5*1000);
  var bookObj = {"bookUrl": detailUrl};
  Logger.info("extracting "+detailUrl);
- bookObj["author"] = await getTextContent(page, AUTHOR_SEL);
- let uploadDateString = await getTextContent(page, UPLOAD_DATE_SEL);
+ // bookObj["author"] = await getTextContent(page, AUTHOR_SEL);
+ // let uploadDateString = await getTextContent(page, UPLOAD_DATE_SEL);
  bookObj["uploadDate"]  = uploadDateString.substring(3, uploadDateString.length);
- bookObj["bookSerial"]= await getTextContent(page, ISBN_SEL);
- bookObj["bookBrief"]  = await getTextContent(page, BOOK_BRIEF_SEL);
- bookObj["category"] = await getTextContent(page, CATEGORY_SEL);
- bookObj["tags"] = await getTextContent(page, TAGS_SEL);
+ // bookObj["bookSerial"]= await getTextContent(page, ISBN_SEL);
+ // bookObj["bookBrief"]  = await getTextContent(page, BOOK_BRIEF_SEL);
+ // bookObj["category"] = await getTextContent(page, CATEGORY_SEL);
+ // bookObj["tags"] = await getTextContent(page, TAGS_SEL);
 
  await page.click(CHECKCODE_SELECTOR);
  await page.keyboard.type(CREDS.checkcode);
@@ -87,7 +87,7 @@ async function crawl(page, detailUrl)
  Logger.info(bookObj.bookName+"@"+bookObj.author);
  Logger.info("book detailed ");
 
- upsertBook(bookObj);
+ // upsertBook(bookObj);
 
 }
 
