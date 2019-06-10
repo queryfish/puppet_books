10db282c38b327190ecde97fb65f8d3a84a07654
McPoet <queryfish@gmail.com>
2019-06-10 14:04:09 +0800

    back to both baid and ctdisk links are missing side?

--- detailCrawler.js
+++ detailCrawler.js
@@ -115,8 +115,8 @@ function assertMongoDB() {
 async function assertBook() {
   assertMongoDB();
   // const conditions = { "baiduUrl": {"$exists": false}}
-  // const conditions = { "$and":[ "$and":[{"ctdiskUrl": {"$exists": false}}, {"baiduUrl":{"$exist":false}}]
-  const conditions = { "$and":[ {"ctdiskUrl": {"$exists": false}},{"bookUrl":{"$exists":true}}]} ;
+  const conditions = { "$and":[ {"$and":[{"ctdiskUrl": {"$exists": false}},{"baiduUrl": {"$exists": false}}]}, {"baiduUrl":{"$exist":false}}];
+  // const conditions = { "$and":[ {"ctdiskUrl": {"$exists": false}},{"bookUrl":{"$exists":true}}]};
   const options = { limit: Config.crawlStep , sort:{"cursorId": -1} };
   var query = Book.find(conditions ,null ,options);
   const result = await query.exec();
