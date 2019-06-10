74300a27a534e9a8ac7be1c1ecbe3e650fdc1090
McPoet <queryfish@gmail.com>
2019-06-10 09:49:51 +0800

    baiduUrl or ctdiskUrl exist would be ok

--- detailCrawler.js
+++ detailCrawler.js
@@ -115,8 +115,8 @@ function assertMongoDB() {
 async function assertBook() {
   assertMongoDB();
   // const conditions = { "baiduUrl": {"$exists": false}} ;
-  const conditions = { "$and":[
-    {"ctdiskUrl": {"$exists": false}},{"bookUrl":{"$exists":true}}]} ;
+  const conditions = { "$and":[ "$or":[{"ctdiskUrl": {"$exists": false}}, {"baiduUrl":{"$exist":false}}]
+    ,{"bookUrl":{"$exists":true}}]} ;
   const options = { limit: Config.crawlStep , sort:{"cursorId": -1} };
   var query = Book.find(conditions ,null ,options);
   const result = await query.exec();
