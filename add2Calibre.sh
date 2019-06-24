a640c90746f7a9308762805a8d46f70838b801f2
McPoet <queryfish@gmail.com>
2019-06-24 18:06:20 +0800

    .

--- add2Calibre.sh
+++ add2Calibre.sh
@@ -10,10 +10,10 @@ do
     echo ${f}
     echo ${epubName}
     `ebook-convert ${f} ${epubName}`
-    bookId=`calibredb add --library-path=~/mycalibre_lib/db ${epubName}`
+    bookId=`calibredb add --library-path=~/mycalibre_lib/db ${epubName} | sed 's/[^0-9]*//g'`
     echo ${bookId}
-    # `calibredb add_format --library-path=~/mycalibre_lib/db ${bookId} ${f}`
+    `calibredb add_format --library-path=~/mycalibre_lib/db ${bookId} ${f}`
     # rm -f ${f} ${epubName}
-    # rm -f ${f}
+    rm -f ${f}
     rm -f ${epubName}
 done
