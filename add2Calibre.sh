1422629b99eb45f460410037e566f6c1875c65f3
McPoet <queryfish@gmail.com>
2019-06-24 17:24:15 +0800

    .

--- add2Calibre.sh
+++ add2Calibre.sh
@@ -7,10 +7,10 @@ do
     # add to calibre
     # `calibredb add --library-path=~/mycalibre_lib/db ${f}`
     # convert to epub
-    epubName=`sed 's/mobi/epub/' "$f" `  # update signature
+    epubName=`echo ${f} | sed 's/mobi/epub/'`  # update signature
     echo ${f}
     echo ${epubName}
-    `ebook-convert "$f" "${epubName}"`
+    # `ebook-convert "$f" "${epubName}"`
     # rm -f ${f} ${epubName}
     # rm -f ${f}
 done
