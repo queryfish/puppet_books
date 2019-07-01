cb4be6ef9655cc9d7c635469542bead2836bb467
McPoet <queryfish@gmail.com>
2019-07-01 22:33:26 +0800

    .

--- doubanCrawler.js
+++ doubanCrawler.js
@@ -102,7 +102,7 @@ async function assertBook() {
   assertMongoDB();
   const conditions = {$and :[
     {"bookSerial": {"$exists": true}},
-    {"bookSerial":{"$ne": ""}},
+    {"bookSerial":{"$ne": null}},
     {"doubanUrl":{"$exists":false}}
   ]}
   const options = { limit:Config.crawlStep };
