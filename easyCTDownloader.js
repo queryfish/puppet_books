4c56984f3ee410c63e39bcbf459834340f4e8b0f
McPoet <queryfish@gmail.com>
2019-07-03 16:01:11 +0800

    .

--- easyCTDownloader.js
+++ easyCTDownloader.js
@@ -11,6 +11,7 @@ const StatsLogger = LOG4JS.stats_logger;
 const CREDS = require('./creds');
 const Configs = require('./configs');
 const OSSPut = require('./saveToAliOSS')
+var execSync = require("exec-sync");
 const MAX_CRAWL_NUM = 200;
 
 var statCount = 0;
@@ -73,63 +74,31 @@ async function downloadBook(bookObj)
     var dl_url = bookObj.ctdownloadUrl;
     var bookUrl = bookObj.bookUrl;
     var bookname = bookObj.bookName;
-    if(dl_url != null && dl_url !="")
-    {
-      Logger.info("start downloading -> "+bookname);
-      const dl = new DownloaderHelper(dl_url, Configs.workingPath+'books/', {fileName:bookname+".mobi", override:false});
 
-      dl.on('end', async () => {
         var ossPath ='books/'+bookname+'.mobi'; //should turn the .mobi to other format
         var localPath = Configs.workingPath+ossPath;
         var movePath = Configs.workingPath+'calibre_tmp/'+bookname+'.mobi';
         console.log(localPath);
         console.log(movePath);
-        let p = OSSPut.put(localPath, ossPath);
-        // let p = OSSPut.put(Configs.workingPath+'books/'+bookname+'.mobi', 'books/'+bookname+'.mobi');
-        if(p == 0)
-        {
-          var update = {downloaded:true, ctdownloadTime:new Date(), savedToAliOSS:true };
-          fs.rename(localPath, movePath, function(e){
-              console.log("file moved to "+movePath);
-              if(e)
-              {
-                console.log(e);
-                return;
-              }
-              var cmdStr = 'sh '+Configs.workingPath+'add2Calibre.sh '+movePath;
-              exec(cmdStr, function(err,stdout,stderr){
-                  if(err)
-                      console.log('get calibre script error:'+stderr);
-                  else
-                      console.log(stdout);
-              });
+        // fs.rename(localPath, movePath, function(e){
+        //     console.log("file moved to "+movePath);
+            // if(e)
+            // {
+            //   console.log(e);
+            //   return;
+            // }
+            var cmdStr = 'sh '+Configs.workingPath+'add2Calibre.sh '+movePath;
+            console.log(cmdStr);
+            execSync(cmdStr);
+            // exec(cmdStr, function(err,stdout,stderr){
+            //     if(err)
+            //         console.log('get calibre script error:'+stderr);
+            //     else
+            //         console.log(stdout);
+            // });
 
-          });
-        }
-        else
-          var update = {downloaded:true, ctdownloadTime:new Date()};
+        // });
 
-        var cond = {"ctdownloadUrl":dl_url};
-        await updateBook(cond,update);
-        // console.log("index"+r.length);
-        // if(r.length == 0)
-        //   mongoose.connection.close();
-        statCount++;
-        Logger.info("DONE downloading "+bookname);
-        StatsLogger.info("Download "+bookname);
-      });
-      dl.on('error', (err) => {
-        Logger.error("Error ...");
-        Logger.error(err);
-      });
-      dl.on('stateChanged', (state) => {Logger.trace("Downloader State changed");Logger.trace(state);});
-      dl.on('progress', (stats)=> {Logger.trace(bookname+" "+Math.floor(stats.progress)+"%");});
-      try {
-        await dl.start();
-      } catch (e) {
-        await unsetCTDownloadUrl(dl_url);
-      }
-    }
 }
 
 
@@ -148,8 +117,10 @@ async function assertBook() {
   assertMongoDB();
   // const conditions = { "baiduUrl": {"$exists": false}} ;
   const conditions = { "$and":[
-    {"downloaded": {"$exists": false}},{"ctdownloadUrl":{"$exists":true}}]} ;
-  const options = { limit:Configs.crawlStep , sort:{"bookSize": 1} };
+                        {"downloaded": {"$exists": true}},
+                        {"ctdownloadUrl":{"$exists":true}}
+                      ]} ;
+  const options = { limit:30 ,skip:60, sort:{"ctdownloadTime": -1} };
   var query = Book.find(conditions ,null ,options);
   const result = await query.exec();
   // Logger.trace(JSON.stringify(result));
@@ -169,6 +140,7 @@ async function automate()
       book = r[i];
       Logger.info("NO. "+i+" book["+book.cursorId+"]: "+book.bookName);
       await downloadBook(book);
+
     }
     StatsLogger.info("CTDownloader Rate "+statCount+"/"+r.length);
 
