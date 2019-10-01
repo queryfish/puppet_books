dc4fad071c3b3933952644fc8fa890b065717ae4
McPoet <queryfish@gmail.com>
2019-10-02 07:57:38 +0800

    .

--- readline.sh
+++ readline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while IFS= read -r line; do
-    echo "Text read from file: $line"
+    # echo "Text read from file: $line"
     # echo $line | sed -n 's/.*upload file success,file:.*.mobi,size:/ \1 /gp'
-    echo $line | sed -n 's/\(.*upload file success,file:\)\(.*\)\(,size.*\)/\2/gp'
+    text=`echo $line | sed -n 's/\(.*upload file success,file:\)\(.*\)\(,size.*\)/\2/gp'`
+    echo $text
 done < "$1"
