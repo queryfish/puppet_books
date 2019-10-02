5fdac222ecdc27b152915acd7a527a480d51972b
McPoet <queryfish@gmail.com>
2019-10-02 08:18:45 +0800

    .

--- readline.sh
+++ readline.sh
@@ -3,5 +3,6 @@ while IFS= read -r line; do
     # echo "Text read from file: $line"
     # echo $line | sed -n 's/.*upload file success,file:.*.mobi,size:/ \1 /gp'
     text=`echo $line | sed -n 's/\(.*upload file success,file:\)\(.*\)\(,size.*\)/\2/gp'`
-    echo $text
+    if [ ! -z "$text"];then
+      echo $text
 done < "$1"
