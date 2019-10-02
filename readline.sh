d18fa29ca91fc831a3aa12c611ce513a051dc7da
McPoet <queryfish@gmail.com>
2019-10-02 08:23:18 +0800

    .

--- readline.sh
+++ readline.sh
@@ -3,6 +3,10 @@ while IFS= read -r line; do
     # echo "Text read from file: $line"
     # echo $line | sed -n 's/.*upload file success,file:.*.mobi,size:/ \1 /gp'
     text=`echo $line | sed -n 's/\(.*upload file success,file:\)\(.*\)\(,size.*\)/\2/gp'`
-    if [ ! -z "$text"];then
+    if [ ! -z "$text" ]; then
       echo $text
+      pathname='./books/'+$text
+      targetname='./calibre_tmp/mobi/'+$text
+      `mv $pathname $targetname`
+    fi
 done < "$1"
